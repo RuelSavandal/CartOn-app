b8a78bcee11220e39042ced7fcabf1733c5b3788
Ruel Savandal <savandalruel@gmail.com>
2021-04-18 16:31:23 +0800

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'SD Brownie Bites',
+            category: 'Food',
+            image: '/images/p1.jpg',
+            price: 50,
+            brand:'Super Delight',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'Delicious Foods and Snacks'
+        },
+        {
+            _id: '2',
+            name: 'BIG ROLLS Cheese & Choco',
+            category: 'Food',
+            image: '/images/p2.jpg',
+            price: 130,
+            brand: 'BIG ROLLS',
+            rating: 5,
+            numReviews: 9,
+            description: 'Delicious Foods and Snacks'
+        },
+        {
+            _id: '3',
+            name: 'SNACKERS Barquillios',
+            category: 'Food',
+            image: '/images/p3.jpg',
+            price: 50,
+            brand: 'SNACKERS',
+            rating: 4,
+            numReviews: 5,
+            description: 'Delicious Foods and Snacks'
+        },
+        {
+            _id: '4',
+            name: 'SD Chocolate Chip Cookies',
+            category: 'Food',
+            image: '/images/p4.jpg',
+            price: 80,
+            brand: 'Super Delight',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'Delicious Foods and Snacks'
+        },
+        {
+            _id: '5',
+            name: 'FIBISCO Black & white Cookies',
+            category: 'Food',
+            image: '/images/p5.jpg',
+            price: 95,
+            brand: 'FIBISCO',
+            rating: 5,
+            numReviews: 16,
+            description: 'Delicious Foods and Snacks'
+        },
+        {
+            _id: '6',
+            name: 'WAFER Choco & Vanilla',
+            category: 'Food',
+            image: '/images/p6.jpg',
+            price: 50,
+            brand: 'WAFER',
+            rating: 5,
+            numReviews: 11,
+            description: 'Delicious Foods and Snacks'
+        },
+    ],
+};
+export default data;
