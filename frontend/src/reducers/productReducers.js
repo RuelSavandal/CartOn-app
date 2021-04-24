536cd08e6d0876170feaf137d0251812795a526c
Ruel Savandal <savandalruel@gmail.com>
2021-04-24 20:07:52 +0800

    share code on github

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,10 @@
 const { 
     PRODUCT_LIST_REQUEST, 
     PRODUCT_LIST_SUCCESS, 
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } = require ('../constants/productConstants');
 
 export const productListReducer = (
@@ -19,4 +22,18 @@ export const productListReducer = (
             return state;
 
     }
-}
+};
+
+
+export const productDetailsReducer = (state = { product:{}, loading: true}, action) =>{
+    switch (action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state
+    }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialtState = {};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
