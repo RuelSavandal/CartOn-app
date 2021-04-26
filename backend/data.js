fb7e9b5905fead6db93f20873fb3305263d240a1
Ruel Savandal <savandalruel@gmail.com>
2021-04-26 11:37:49 +0800

    share code on github

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+    users: [
+        {
+            name: 'Ruel',
+            email: 'savandalruel@gmail.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'User',
+            email: 'user@gmail.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        }
+    ],
     products: [
         {
             _id: '1',
