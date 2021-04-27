dc70502d7419b104abe97bf5822e4f3ac5f092de
Ruel Savandal <savandalruel@gmail.com>
2021-04-27 13:14:30 +0800

    share code on github

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,11 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+    CART_ADD_ITEM, 
+    CART_ADD_ITEM_FAIL,
+    CART_EMPTY,
+    CART_REMOVE_ITEM,
+    CART_SAVE_PAYMENT_METHOD,
+    CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: []}, action) =>{
     switch(action.type) {
@@ -18,9 +25,16 @@ export const cartReducer = (state = { cartItems: []}, action) =>{
         case CART_REMOVE_ITEM:
             return {
                 ...state,
-                error:'',
                 cartItems: state.cartItems.filter((x) => x.product !== action.payload),
             };
+            case CART_SAVE_SHIPPING_ADDRESS:
+                return { ...state, shippingAddress: action.payload };
+              case CART_SAVE_PAYMENT_METHOD:
+                return { ...state, paymentMethod: action.payload };
+              case CART_ADD_ITEM_FAIL:
+                return { ...state, error: action.payload };
+              case CART_EMPTY:
+                return { ...state, error: '', cartItems: [] };
         default:
             return state;
     }
