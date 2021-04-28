ea9546ceda7847479b2661ef382326d0c5240eeb
Ruel Savandal <savandalruel@gmail.com>
2021-04-28 18:05:26 +0800

    first commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema(
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
         isAdmin: {type: Boolean, default: false, required: true},
-        
     }, 
     {
         timestamps: true,
